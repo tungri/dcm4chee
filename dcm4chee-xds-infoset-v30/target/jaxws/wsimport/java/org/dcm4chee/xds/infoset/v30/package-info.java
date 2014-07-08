09da1bdbc971ee2ea8f1517d036a441f3de98717
Satya <Satyanarayana.gandham@telehealthindia.com>
2014-07-08 18:16:26 +0530

    commit

---
+++ dcm4chee-xds-infoset-v30/target/jaxws/wsimport/java/org/dcm4chee/xds/infoset/v30/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * IHE XDS.b Document Repository
+ * 
+ */
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.dcm4chee.xds.infoset.v30;
